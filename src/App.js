5ca9795a0adbb795b02355bb027191789d4094da
dell <khasyu@mail.ru>
2019-11-23 13:23:12 +0600

    Добавление фото и компонента

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import lavash from './img/lavash.jpg';
+import burger from './img/burger.jpg';
+import pita from './img/pita.jpg';
+import pizza from './img/pizza.jpg';
+import cake from './img/cake.jpg';
+import orange from './img/orange.jpg';
 import './App.css';
 
-export const Dish = [
-  {title: 'Shaurma-Falafel', price: 155},
-  {title: 'Burger-Falafel', price: 110},
-  {title: 'Pita-Falafel', price: 120},
-  {title: 'Pizza-Vegetables', price: 550},
-  {title: 'Cake-Coconut', price: 1500},
-  {title: 'Juice-Orange', price: 210}
+export const Food = [
+  {title: 'Shaurma-Falafel', price: 155, recipe:'chickpeas, salad, tofu sauce, lavash', img:lavash},
+  {title: 'Burger-Falafel', price: 110, recipe:'chickpeas, cabbage leaves, bread', img:burger},
+  {title: 'Pita-Falafel', price: 120, recipe:'chickpeas, flapjack, tomato paste', img:pita},
+  {title: 'Pizza-Vegetables', price: 550, recipe:'dough, mushrooms, tofu, tomatoes, bell pepper', img:pizza},
+  {title: 'Cake-Coconut', price: 1500, recipe:'dough, coconut milk, syrup, fruits', img:cake},
+  {title: 'Juice-Orange', price: 210, recipe:'freshly squeezed orange juice', img:orange}
 ];
 
 class App extends Component {
--- src/Components/Menu/Dish/Dish.js
+++ src/Components/Menu/Dish/Dish.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const Dish = () => {
+import {Food} from '../../../App';
 
+const Dish = props => {
+    const food = Food.find(f => f.title === props.title);
+    return(
+        <div onClick={props.addFood} className='box'>
+            <img className='imgDish' src={food.img} alt='Photo_Food'/>
+            <h5>{props.title}</h5>
+            <p>{props.recipe}</p>
+            <p>{props.price}</p>
+        </div>
+    )
 };
 
 export default Dish;
